099c88d240829a3d9866c54e36d523510977515e
Caitlin Lauer <caitlinrinn@gmail.com>
2018-04-23 17:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Rinn"
-var height = "57"
+var height = 57
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1 )
+console.log(height) + 1 )
